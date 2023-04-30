4f63769ae1005636f5e70090a3e3be9f84cb923e
Mateen <zahidx297@yahoo.com>
2023-04-30 15:28:21 -0400

    add knight image asset

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ for (let i = 8; i >= 1; i--) {
         node.classList.add('square');
         if((i % 2) === (j % 2))
             node.classList.add('dark');
+        if (i === 4 && j === 5) {
+            node.classList.add('knight'); // DEBUG
+        }
         node.textContent = `${i},${j}`; // DEBUG
         boardNode.appendChild(node);
     }
 }
+
+// Attributions
+// <a href="https://www.flaticon.com/free-icons/chess" title="chess icons">Chess icons created by apien - Flaticon</a>
