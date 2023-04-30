9061a15fddb5ef006ffb3af9568877facb7a3bfd
Mateen <zahidx297@yahoo.com>
2023-04-30 06:19:32 -0400

    add basic squares

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css'
 
-console.log('Hello, World!');
+// add board squares
+let boardNode = document.getElementById('board');
+for (let i = 8; i >= 1; i--) {
+    for (let j = 1; j <= 8; j++) {
+        let node = document.createElement('div');
+        node.classList.add('square');
+        if((i % 2) === (j % 2))
+            node.classList.add('dark');
+        node.textContent = `${i},${j}`; // DEBUG
+        boardNode.appendChild(node);
+    }
+}
