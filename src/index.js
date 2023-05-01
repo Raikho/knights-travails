0d71606b096b45deda542ce0aa8d52624508b437
Mateen <zahidx297@yahoo.com>
2023-05-01 05:11:27 -0400

    add initial tree algorithm

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import Tree from './tree.js'
 // add board squares
 DOM.createSquares(document.getElementById('board'));
 let board = new Board();
-board.setKnight(4,6);
-board.setTarget(7,8);
+board.setKnight(4,7);
+board.setTarget(6,2);
 
 let tree = new Tree(board.knight, board.target);
 for (let child of tree.knight.children) {
     board.setOutline(child.x, child.y);
+    console.log(child);
+    for (let child2 of child.children) {
+        board.setOutline(child2.x, child2.y);
+    }
 }
 
 // Attributions
--- src/tree.js
+++ src/tree.js
@@ -15,14 +15,28 @@ export default class Tree {
         this.target = new Node(target);
 
         this.createChildren(this.knight);
+        for (let child of this.knight.children)
+            this.createChildren(child);
     }
 
     createChildren(node) {
         for (let move of this.moves) {
             let x = node.x + move[0];
-            let y = node.x + move[1];
+            let y = node.y + move[1];
             if (x < 1 || x > 8 || y < 1 || y > 8)
                 continue;
+
+            if (x == this.knight.x && y == this.knight.y)
+                continue;
+            if (x == this.target.x && y == this.target.y)
+                continue;
+
+            let dx = Math.abs(this.target.x - node.x) - Math.abs(this.target.x - x);
+            let dy = Math.abs(this.target.y - node.y) - Math.abs(this.target.y - y);
+            //console.log(`knight ${x},${y} | dx: ${dx} | dy: ${dy} | closer: ${(dx>0||dy>0)}`);
+            if (!(dx>0 || dy>0) || dx<=-2 || dy <=-2)
+                continue;
+
             node.children.push(new Node([x,y]));
         }
     }
