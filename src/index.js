c70b32986e9cc411eb0d67bc9cfe237d56a083c8
Mateen <zahidx297@yahoo.com>
2023-05-01 00:58:44 -0400

    create tree class

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ for (let i = 8; i >= 1; i--) {
     for (let j = 1; j <= 8; j++) {
         let node = document.createElement('div');
         node.classList.add('square');
-        node.dataset.x = i;
-        node.dataset.y = j;
+        node.dataset.y = i;
+        node.dataset.x = j;
 
         if((i % 2) === (j % 2))
             node.classList.add('dark');
@@ -45,12 +45,42 @@ function setOutline(x, y) {
     node.dataset.type='outline';
 }
 
-clearKnight();
-setKnight(2,2);
-setKnight(2,3);
-setKnight(2,1);
-setTarget(2,3);
-setOutline(3,3);
+setKnight(2,4);
+setTarget(8,3);
+
+class Node {
+    constructor(coords) {
+        this.x = coords[0];
+        this.y = coords[1];
+        this.children = [];
+    }
+}
+
+class Tree {
+    constructor(knightCoords, targetCoords) {
+        this.moves = [[2,1],[1,2],[-1,2],[-2,1],
+                      [-2,-1],[-1,-2],[1,-2],[2,-1]];
+        this.start = new Node(knightCoords);
+        this.end = new Node(targetCoords);
+        this.children = [];
+        this.createChildren();
+    }
+    createChildren() {
+        for (let move of this.moves) {
+            let x = this.start.x + move[0];
+            let y = this.start.y + move[1];
+            if (x < 1 || x > 8 || y < 1 || y > 8)
+                continue;
+            this.children.push([x,y]);
+        }
+        console.log(this.children);
+    }
+}
+
+let tree = new Tree([2,4], [8,3]);
+for (let child of tree.children) {
+    setOutline(child[0], child[1]);
+}
 
 // Attributions
 // <a href="https://www.flaticon.com/free-icons/chess" title="chess icons">Chess icons created by apien - Flaticon</a>
