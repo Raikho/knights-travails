26e62ac6a66f9f64d56fd57eb364879d0aa19d50
Mateen <zahidx297@yahoo.com>
2023-05-01 00:42:19 -0400

    add icon functionality

--- src/index.js
+++ src/index.js
@@ -21,18 +21,37 @@ function getLetter(num) {
     return String.fromCharCode(97 + num);
 }
 
-function clearKnights() {
-    for (let node of document.querySelectorAll('.knight'))
-        node.classList.remove('knight');
+function clearKnight() {
+    for (let node of document.querySelectorAll('[data-type="knight"]'))
+        node.dataset.type = '';
 }
 
 function setKnight(x, y) {
+    let prevKnight = document.querySelector('[data-type="knight"]');
+    if (prevKnight)
+        prevKnight.dataset.type = '';
+
     let node = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
-    node.classList.add('knight');
+    node.dataset.type='knight';
 }
 
-clearKnights();
+function setTarget(x, y) {
+    let node = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+    node.dataset.type='target';
+}
+
+function setOutline(x, y) {
+    let node = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+    node.dataset.type='outline';
+}
+
+clearKnight();
 setKnight(2,2);
+setKnight(2,3);
+setKnight(2,1);
+setTarget(2,3);
+setOutline(3,3);
 
 // Attributions
 // <a href="https://www.flaticon.com/free-icons/chess" title="chess icons">Chess icons created by apien - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/target" title="target icons">Target icons created by Creative Stall Premium - Flaticon</a>
