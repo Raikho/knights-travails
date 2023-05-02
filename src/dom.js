cec34ee4a7fcb8974c30a317ab0071f446c275db
Mateen <zahidx297@yahoo.com>
2023-05-02 08:11:47 -0400

    add clearing of results

--- src/dom.js
+++ src/dom.js
@@ -3,7 +3,7 @@ const DOM = {};
 DOM.createSquares = function(parentNode) {
     for (let i = 8; i >= 1; i--) {
         for (let j = 1; j <= 8; j++) {
-            let node = this.createDiv(parentNode);
+            let node = createDiv(parentNode);
             node.classList.add('square');
             node.dataset.x = j;
             node.dataset.y = i;
@@ -25,19 +25,29 @@ DOM.removeClickCallback = function(callback) {
 
 DOM.writeSolution = function(solution) {
     let node = document.getElementById('results-container')
+    clearNode(node);
     for (let i = 0; i < solution.length; i++) {
         let x = solution[i][0];
         let y = solution[i][1];
         let label = (i == 0) ? 'Start' : `Move ${i}`;
-        this.createDiv(node, `${label}: ${x}, ${y}\n`);
+        createDiv(node, `${label}: [${toLetters(x)}${y}]`);
     }
 }
 
-DOM.createDiv = function(parentNode, text) {
+function createDiv(parentNode, text) {
     let node = document.createElement('div');
     node.textContent = text;
     parentNode.appendChild(node);
     return node;
+}
+
+function clearNode(node) {
+    while (node.hasChildNodes())
+        node.removeChild(node.firstChild);
+}
+
+function toLetters(num) {
+    return String.fromCharCode(96 + num);
 }
 
 export default DOM;
