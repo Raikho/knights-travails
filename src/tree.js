b7295eb22fe3ae9350af0396cb3431c4696443be
Mateen <zahidx297@yahoo.com>
2023-05-02 06:21:00 -0400

    rearrange tree class

--- src/tree.js
+++ src/tree.js
@@ -5,50 +5,34 @@ class Node {
         this.parent = parent;
         this.children = [];
     }
-    get inBounds() {
-        return (this.x < 1 || this.x > 8 || this.y < 1 || this.y > 8) ? false : true;
-    }
-    isTouching(node) {
-        return (this.x === node.x && this.y === node.y) ? true : false;
-    }
 }
 
 export default class Tree {
+    static moves = [[2,1],[1,2],[-1,2],[-2,1],[-2,-1],[-1,-2],[1,-2],[2,-1]];
+
     constructor(knight, target) {
-        this.moves = [[2,1],[1,2],[-1,2],[-2,1],
-                     [-2,-1],[-1,-2],[1,-2],[2,-1]];
-        this.knight = new Node(knight);
-        this.target = new Node(target);
+        let lastChild = this.findTarget(new Node(knight), new Node(target));
 
-        // find target by creating tree
-        this.lastChild = this.findTarget(this.knight, this.target);
-
-        // get array;
         this.solution = [];
-        let child = this.lastChild;
+        let child = lastChild;
         while (child.parent) {
-            this.solution.unshift([child.x, child.y,]);
+            this.solution.unshift([child.x, child.y]);
             child = child.parent;
         }
         this.solution.unshift([child.x, child.y,]);
-
-        console.log('knight: ', this.knight);
-        console.log('last child: ', this.lastChild);
-        console.log('solution: ', this.solution);
     }
 
     findTarget(start, end) {
         let queue = [start];
         while (queue.length) {
             let node = queue.shift();
-            for (let move of this.moves) {
+            for (let move of Tree.moves) {
                 let child = new Node([node.x+move[0], node.y+move[1]], node);
 
-                if (!child.inBounds)
+                if (child.x < 1 || child.x > 8 || child.y < 1 || child.y > 8)
                     continue;
-                if (child.isTouching(end))
+                if (child.x == end.x && child.y == end.y)
                     return child;
-
                 node.children.push(child);
                 queue.push(child);
             }
