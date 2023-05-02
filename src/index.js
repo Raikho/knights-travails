3598c7be7e83fbd3b73108fba82d20593558bb4b
Mateen <zahidx297@yahoo.com>
2023-05-02 05:45:25 -0400

    and coordinate randomization

--- src/index.js
+++ src/index.js
@@ -6,26 +6,24 @@ import Tree from './tree.js'
 // add board squares
 DOM.createSquares(document.getElementById('board'));
 let board = new Board();
-board.setKnight(7,7);
-board.setTarget(8,2);
+
+function getRandomInt() {
+    return Math.trunc(Math.random()*8) + 1
+}
+
+let knightX = getRandomInt();
+let targetX = getRandomInt();
+while (targetX == knightX)
+    targetX = getRandomInt();
+
+board.setKnight(knightX, getRandomInt());
+board.setTarget(targetX, getRandomInt());
 
 let tree = new Tree(board.knight, board.target);
 let solution = tree.solution;
 
 for (let i = 1; i < solution.length - 1; i++) {
     board.setOutline(solution[i][0], solution[i][1]);
-}
-
-for (let child of tree.knight.children) {
-    break;
-    board.setOutline(child.x, child.y);
-    // console.log(child);
-    for (let child2 of child.children) {
-        board.setOutline(child2.x, child2.y);
-        for (let child3 of child2.children) {
-            board.setOutline(child3.x, child3.y);
-        }
-    }
 }
 
 
