c2168feb471eef06cd91d3a500399dd7ad6dc190
Mateen <zahidx297@yahoo.com>
2023-05-02 06:25:35 -0400

    group main script into functions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import DOM from './dom.js'
 import Board from './board.js'
 import Tree from './tree.js'
 
-// add board squares
 DOM.createSquares(document.getElementById('board'));
 let board = new Board();
 
@@ -11,21 +10,25 @@ function getRandomInt() {
     return Math.trunc(Math.random()*8) + 1
 }
 
-let knightX = getRandomInt();
-let targetX = getRandomInt();
-while (targetX == knightX)
-    targetX = getRandomInt();
+function setRandomPieces(board) {
+    let knightX = getRandomInt();
+    let targetX = getRandomInt();
+    while (targetX == knightX)
+        targetX = getRandomInt();
 
-board.setKnight(knightX, getRandomInt());
-board.setTarget(targetX, getRandomInt());
-
-let tree = new Tree(board.knight, board.target);
-let solution = tree.solution;
-
-for (let i = 1; i < solution.length - 1; i++) {
-    board.setOutline(solution[i][0], solution[i][1]);
+    board.setKnight(knightX, getRandomInt());
+    board.setTarget(targetX, getRandomInt());
 }
 
+function drawSolution(board) {
+    let tree = new Tree(board.knight, board.target);
+    let solution = tree.solution;
+    for (let i = 1; i < solution.length - 1; i++)
+        board.setOutline(solution[i][0], solution[i][1]);
+}
+
+setRandomPieces(board);
+drawSolution(board);
 
 // Attributions
 // <a href="https://www.flaticon.com/free-icons/chess" title="chess icons">Chess icons created by apien - Flaticon</a>
