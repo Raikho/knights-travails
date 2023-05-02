4bf093fc7f7848b4e9dcb8f9dbe59f3aaa472cc6
Mateen <zahidx297@yahoo.com>
2023-05-02 07:24:01 -0400

    fix button functionality

--- src/index.js
+++ src/index.js
@@ -10,6 +10,31 @@ function getRandomInt() {
     return Math.trunc(Math.random()*8) + 1
 }
 
+document.getElementById('setKnight').addEventListener('click', () => {
+    board.clearSolution();
+    DOM.addClickCallback(setKnightFromClick);
+})
+document.getElementById('setTarget').addEventListener('click', () => {
+    board.clearSolution();
+    DOM.addClickCallback(setTargetFromClick);
+})
+document.getElementById('randomize-pieces').addEventListener('click', () => {
+    setRandomPieces(board);
+})
+document.getElementById('find-path').addEventListener('click', () => {
+    drawSolution(board);
+})
+
+function setKnightFromClick(event) {
+    let node = event.target;
+    board.setKnight(Number(node.dataset.x), Number(node.dataset.y));
+    DOM.removeClickCallback(setKnightFromClick);
+}
+function setTargetFromClick(event) {
+    let node = event.target;
+    board.setTarget(Number(node.dataset.x), Number(node.dataset.y));
+    DOM.removeClickCallback(setTargetFromClick);
+}
 function setRandomPieces(board) {
     board.clearSolution();
 
@@ -21,40 +46,12 @@ function setRandomPieces(board) {
     board.setKnight(knightX, getRandomInt());
     board.setTarget(targetX, getRandomInt());
 }
-
 function drawSolution(board) {
     let tree = new Tree(board.knight, board.target);
     let solution = tree.solution;
     for (let i = 1; i < solution.length - 1; i++)
         board.setOutline(solution[i][0], solution[i][1]);
 }
-
-document.getElementById('setKnight').addEventListener('click', () => {
-    board.clearSolution();
-    DOM.addClickCallback(setKnightFromClick);
-})
-document.getElementById('setTarget').addEventListener('click', () => {
-    board.clearSolution();
-    DOM.addClickCallback(setTargetFromClick);
-})
-function setKnightFromClick(event) {
-    let node = event.target;
-    board.setKnight(Number(node.dataset.x), Number(node.dataset.y));
-    DOM.removeClickCallback(setKnightFromClick);
-}
-function setTargetFromClick(event) {
-    let node = event.target;
-    board.setTarget(Number(node.dataset.x), Number(node.dataset.y));
-    DOM.removeClickCallback(setTargetFromClick);
-}
-
-document.getElementById('randomize-pieces').addEventListener('click', () => {
-    setRandomPieces(board);
-})
-
-document.getElementById('find-path').addEventListener('click', () => {
-    drawSolution(board);
-})
 
 setRandomPieces(board);
 drawSolution(board);
